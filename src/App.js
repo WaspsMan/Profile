274951896d8db6f2d0cc72d12e13d5e46e037166
waspsman <ta_tor67@hotmail.com>
2018-08-14 20:55:46 +0700

    'addTimeline'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import 'antd/dist/antd.css';
 import 'moment/locale/th';
 import { Layout } from 'antd'
-import { Navbar, Profile, Experience } from './Components';
+import {  Profile, Experience } from './Components';
 
 class App extends Component {
   render() {
@@ -13,9 +13,9 @@ class App extends Component {
         <Layout.Content className="backGroud1">
           <Profile/>
         </Layout.Content>
-        {/* <Layout.Content className="backGroud2">
+        <Layout.Content className="backGroud2">
           <Experience/>
-        </Layout.Content> */}
+        </Layout.Content>
       </Layout>
     );
   }
